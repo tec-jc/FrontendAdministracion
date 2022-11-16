698480ae10bb698ea4421bc6059aecdbfdc31d82
JC Tula <jc_tula@outlook.com>
2022-11-16 12:49:55 -0600

    Acciones index y agregar de municipios

--- Models/Municipio.cs
+++ Models/Municipio.cs
@@ -1,11 +1,18 @@
-﻿namespace DeptosES.AdministradorBlazor.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DeptosES.AdministradorBlazor.Models
 {
     public class Municipio
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "El departamento es requerido")]
         public int IdDepartamento { get; set; }
+        [Required(ErrorMessage = "El nombre es requerido")]
         public string Nombre { get; set; }
+        [Required(ErrorMessage = "La descripción es requerida")]
         public string Descripcion { get; set; }
+        [Required(ErrorMessage = "La imagen es requerida")]
         public string Imagen { get; set; }
+        public Departamento Departamento { get; set; }
     }
 }
