ffd440043504597e275ea8af866d1627f6f9b04e
JC Tula <jc_tula@outlook.com>
2022-11-17 11:37:52 -0600

    CRUD completo de Roles

--- Models/Rol.cs
+++ Models/Rol.cs
@@ -1,8 +1,11 @@
-﻿namespace DeptosES.AdministradorBlazor.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DeptosES.AdministradorBlazor.Models
 {
     public class Rol
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "El nombre es requerido")]
         public string Nombre { get; set; }
     }
 }
